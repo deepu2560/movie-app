045611d807092b07ddbcbfb46c2579a2b6b74956
unknown <deepanshugulia256@gmail.com>
2022-06-27 14:37:59 +0530

    working

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,39 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setsearch] = useState("spiderman");
+
+  function handleinput({ value }) {
+    setsearch(() => value);
+  }
+
+  function handlesearch(key) {
+    if (key === "Enter") {
+      searchMovie();
+    }
+  }
+
+  function searchMovie() {
+    axios
+      .get(`http://www.omdbapi.com/?t=${search}&apikey=21c58bc6`)
+      .then((res) => {
+        console.log(res.data);
+      });
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <input
+        type="text"
+        placeholder="Search..."
+        onChange={(event) => handleinput(event.target)}
+        onKeyPress={(event) => handlesearch(event.key)}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
