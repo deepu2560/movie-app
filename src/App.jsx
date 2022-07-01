84d8cfd78b35c93a3ade6669d294d6938397a7d6
unknown <deepanshugulia256@gmail.com>
2022-07-01 16:43:35 +0530

    changed ui for search

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react";
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import axios from "axios";
 
@@ -10,33 +9,43 @@ function App() {
     setsearch(() => value);
   }
 
-  function handlesearch(key) {
-    if (key === "Enter") {
-      searchMovie();
-    }
-  }
+  // useEffect(() => {
 
-  function searchMovie() {
-    axios
-      .get(`http://www.omdbapi.com/?t=${search}&apikey=21c58bc6`)
-      .then((res) => {
-        console.log(res.data);
-      });
-  }
+  // }, [search]);
+
+  const [data, setdata] = useState([
+    {
+      adult: false,
+      backdrop_path: "/iQFcwSGbZXMkeyKrxbPnwnRo5fl.jpg",
+      genre_ids: [28, 12, 878],
+      id: 634649,
+      original_language: "en",
+      original_title: "Spider-Man: No Way Home",
+      overview:
+        "Peter Parker is unmasked and no longer able to separate his normal life from the high-stakes of being a super-hero. When he asks for help from Doctor Strange the stakes become even more dangerous, forcing him to discover what it truly means to be Spider-Man.",
+      popularity: 1927.454,
+      poster_path: "/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg",
+      release_date: "2021-12-15",
+      title: "Spider-Man: No Way Home",
+      video: false,
+      vote_average: 8.1,
+      vote_count: 13846,
+    },
+  ]);
 
   return (
     <div className="App">
       <h1>Movie app</h1>
       <input
         type="text"
-        placeholder="Press enter to search movie..."
+        placeholder="Search movie here..."
         onChange={(event) => handleinput(event.target)}
-        onKeyPress={(event) => handlesearch(event.key)}
         id="search-input"
       />
       <div id="movie-display-div">
-        <img src="" alt="" />
-        <div></div>
+        {data.map((elem) => (
+          <div></div>
+        ))}
       </div>
     </div>
   );
